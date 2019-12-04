ff06a091deac87edcd00a4c30d5916d42eff97f6
=CookieMonster <18721775528@163.com>
2019-12-04 11:03:35 +0800

    bugfix在线和报警模块

--- src/components/Monitor/DeviceDetail/dataWarn.vue
+++ src/components/Monitor/DeviceDetail/dataWarn.vue
@@ -4,8 +4,8 @@
             <div class="text-left" style="font-size:16px;font-family:'中易宋体';font-weight: bold;">{{title}}</div>
         </div> 
         <div>
-        <img v-show="value=='0'" src="../../../assets/bOn.png"  width="40" height="40"/>
-        <img v-show="value=='1'" src="../../../assets/bWarn.png"  width="40" height="40"/> 
+        <img v-show="value==0" src="../../../assets/bOn.png"  width="40" height="40"/>
+        <img v-show="value==1" src="../../../assets/bWarn.png"  width="40" height="40"/> 
         <!-- <input type="text"  :value="value" readonly="true" class="textInput" > -->
         </div> 
     </div>
@@ -27,11 +27,11 @@
 export default {
     name:'dataShow',
     created(){
-        alert(value)
+
     },
     props:{
         value:{
-            type:String,
+            type:Number,
             require:true
         },
         title: String,
